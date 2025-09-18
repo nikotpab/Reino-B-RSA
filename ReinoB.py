8d33fd68dc496ee5aa253d732e5f2e14702d3c9a
Jjpablo28 <jpr.rod06@gmail.com>
2025-09-18 07:59:59 -0500

    implementacion funciones RSA
    Implementacion funciones de cifrar y descifrar con librerias (pycryptodome) de Python para el cifrado RSA

--- ReinoB.py
+++ ReinoB.py
@@ -1,4 +1,9 @@
 import tkinter as tk
+#pip install pycryptodome
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import base64
+
 
 root = tk.Tk()
 
@@ -7,3 +12,13 @@ root.resizable(False, False)
 root.minsize(1400, 700)
 
 root.mainloop()
+
+
+def cifrar_texto(mensaje: str) -> str:
+    cifrado = cipher_rsa.encrypt(mensaje.encode("utf-8"))
+    return base64.b64encode(cifrado).decode("utf-8")  
+
+def descifrar_texto(cifrado_b64: str) -> str:
+    cifrado = base64.b64decode(cifrado_b64)
+    descifrado = decipher_rsa.decrypt(cifrado)
+    return descifrado.decode("utf-8")
